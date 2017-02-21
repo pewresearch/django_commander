7f6491c8eb4aba599644878ff56172d6afc70944
pewresearch <patrickvankessel@gmail.com>
2017-02-21 15:29:55 -0500

    renamed to django_commander and updated the subcommand functions

--- settings.py
+++ settings.py
@@ -13,11 +13,11 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 from django.db import models
 
-if not getattr(settings, 'LOADER_DIR', None):
-    LOADER_DIR = None
-if not getattr(settings, 'LOADER_BASE_MODEL', None):
-    LOADER_BASE_MODEL = models.Model
-if not getattr(settings, 'LOADER_BASE_MANAGER', None):
+if not getattr(settings, 'COMMAND_DIR', None):
+    COMMAND_DIR = None
+if not getattr(settings, 'COMMAND_BASE_MODEL', None):
+    COMMAND_BASE_MODEL = models.Model
+if not getattr(settings, 'COMMAND_BASE_MANAGER', None):
     from pewtils.django.managers import BasicManager
-    LOADER_BASE_MANAGER = BasicManager
+    COMMAND_BASE_MANAGER = BasicManager
     # LOADER_BASE_MANAGER = models.QuerySet
--- utils.py
+++ utils.py
@@ -2,22 +2,12 @@ import pkgutil, importlib, re
 
 from django.conf import settings
 
+from pewtils import extract_attributes_from_folder_modules
 
-def get_project_loaders():
 
-    name_split = settings.LOADER_DIR.split(settings.SITE_NAME)
-    name = settings.SITE_NAME + re.sub(r"[/\\]", '.', name_split[-1])
-    path = [settings.LOADER_DIR]
-    loaders = {}
-    path = pkgutil.extend_path(path, name)
-    for importer, modname, ispkg in pkgutil.walk_packages(path=path, prefix=name + '.'):
-        if not ispkg:
-            module = importlib.import_module(modname)
-            if hasattr(module, "Loader"):
-                loader = getattr(module, "Loader")
-                loaders[loader.name] = loader
+def get_project_commands():
 
-    return loaders
+    return extract_attributes_from_folder_modules(settings.COMMAND_DIR, "Command", include_subdirs=True, concat_subdir_names=True)
 
 
 class MissingDependencyException(Exception):
