fce72a8c1f8d51c58d792bdc856bc0446a2c9559
pewresearch <patrickvankessel@gmail.com>
2017-03-10 10:10:43 -0500

    cleaned up and simplified settings

--- django_commander/settings.py
+++ django_commander/settings.py
@@ -2,24 +2,17 @@
 import os
 
 from django.conf import settings
-from django.db import models
+
+from pewtils.django.abstract_models import BasicExtendedModel
+from pewtils.django.managers import BasicManager
+
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-# Copied from django_extensions as a boilerplate example
-# REPLACEMENTS = {
-# }
-# add_replacements = getattr(settings, 'EXTENSIONS_REPLACEMENTS', {})
-# REPLACEMENTS.update(add_replacements)
 
-from django.db import models
-
-if not getattr(settings, 'DJANGO_COMMANDER_COMMAND_FOLDERS', None):
-    DJANGO_COMMANDER_COMMAND_FOLDERS = []
-if not getattr(settings, 'DJANGO_COMMANDER_BASE_MODEL', None):
-    # DJANGO_COMMANDER_BASE_MODEL = models.Model
-    from pewtils.django.abstract_models import BasicExtendedModel
-    DJANGO_COMMANDER_BASE_MODEL = BasicExtendedModel
-if not getattr(settings, 'DJANGO_COMMANDER_BASE_MANAGER', None):
-    from pewtils.django.managers import BasicManager
-    DJANGO_COMMANDER_BASE_MANAGER = BasicManager
-    # LOADER_BASE_MANAGER = models.QuerySet
+for setting, default in [
+    ("DJANGO_COMMANDER_COMMAND_FOLDERS", []),
+    ("DJANGO_COMMANDER_BASE_MODEL", BasicExtendedModel),
+    ("DJANGO_COMMANDER_BASE_MANAGER", BasicManager)
+]:
+    if not getattr(settings, setting, None):
+        globals()[setting] = default
