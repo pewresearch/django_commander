79ccf0900a2b024c61207d1b608cb2fd49b7c666
pewresearch <patrickvankessel@gmail.com>
2017-03-10 10:09:48 -0500

    added a celery task wrapper function

--- django_commander/utils.py
+++ django_commander/utils.py
@@ -1,22 +1,16 @@
-import pkgutil, importlib, re
-
-from multiprocessing import Process
-
-from django.conf import settings
-
-from pewtils.django import reset_django_connection
+from celery import shared_task
 
 
 class MissingDependencyException(Exception):
     pass
 
-def run_command_async(command_name, params):
 
-    def run_command(name, **params):
-        reset_django_connection()
+@shared_task
+def run_command_task(command_name, params):
+
+    try:
         from django_commander.commands import commands
-        commands[name](**params).run()
-
-    p = Process(target=run_command, args=(command_name, ), kwargs=params)
-    p.start()
-    p.join()
+        commands[command_name](**params).run()
+        return "Success: {}".format(command_name)
+    except Exception as e:
+        return e
