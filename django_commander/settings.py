6d794ccb5cee2bd595789120e0534a969698839a
pvankessel <pvankessel@pewresearch.org>
2017-03-13 15:44:21 -0400

    removed variable base models and managers, thats not a good pattern

--- django_commander/settings.py
+++ django_commander/settings.py
@@ -3,16 +3,11 @@ import os
 
 from django.conf import settings
 
-from pewtils.django.abstract_models import BasicExtendedModel
-from pewtils.django.managers import BasicExtendedManager
-
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 for setting, default in [
-    ("DJANGO_COMMANDER_COMMAND_FOLDERS", []),
-    ("DJANGO_COMMANDER_BASE_MODEL", BasicExtendedModel),
-    ("DJANGO_COMMANDER_BASE_MANAGER", BasicExtendedManager)
+    ("DJANGO_COMMANDER_COMMAND_FOLDERS", [])
 ]:
     if not getattr(settings, setting, None):
         globals()[setting] = default
