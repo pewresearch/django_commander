7330ea233adc0fbe32fb5ef96ffe28972f2fe06f
pvankessel <pvankessel@pewresearch.org>
2017-03-17 09:02:31 -0400

    updated setup.py to include static files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django_commander',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Easily create, organize, and log management commands and subcommands',
     long_description = 'http://labs.pewresearch.tech/docs/libs/django_commander',
     url = 'https://github.com/pewresearch/django_commander',
@@ -13,6 +13,7 @@ setup(
         'pewtils'
     ],
     packages = find_packages(exclude = ['contrib', 'docs', 'tests']),
+    include_package_data=True,
     classifiers = [
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 1 - Planning',
