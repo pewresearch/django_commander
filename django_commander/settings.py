7a4423c4a5db722b96a60106fd0f7e92d5f4ae6d
pvankessel <pvankessel@pewresearch.org>
2017-03-30 12:54:46 -0400

    added basic interface for exploring commands and logs

--- django_commander/settings.py
+++ django_commander/settings.py
@@ -13,3 +13,19 @@ for setting, default in [
         globals()[setting] = default
     else:
         globals()[setting] = getattr(settings, setting)
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages'
+            ]
+        }
+    }
+]
--- django_commander/views.py
+++ django_commander/views.py
@@ -1,3 +1,28 @@
-# TODO: make some basic informational dashboards here
-# borrow from the mediastream interface
-# just show the most recent commands, and the objects the affected
+import datetime
+
+from django.shortcuts import render
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
+from django_commander.models import Command, CommandLog
+
+
+@login_required
+def home(request):
+
+    commands = []
+    for c in Command.objects.all():
+        commands.append({"command": c, "latest_log": c.logs.order_by("-start_time")[0]})
+    commands = sorted(commands, key=lambda x: x['latest_log'].start_time, reverse=True)
+
+    return render(request, 'django_commander/index.html', {"commands": commands})
+
+
+@login_required
+def view_command(request, command_id):
+
+    command = Command.objects.get(pk=command_id)
+    logs = command.logs.order_by("-start_time")
+
+    return render(request, 'django_commander/command.html', {"command": command, "logs": logs})
