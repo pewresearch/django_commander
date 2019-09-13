694b0aa453aded1d10d0fc73bd61d5bc2799234b
pvankessel <pvankessel@pewresearch.org>
2019-09-13 16:36:52 -0400

    cleaned up testapp settings

--- testapp/settings.py
+++ testapp/settings.py
@@ -27,29 +27,9 @@ DATABASES = {
 
 SECRET_KEY = "testing"
 
+### DJANGO_COMMANDER SETTINGS
+
 DJANGO_COMMANDER_COMMAND_FOLDERS = [
     os.path.abspath(os.path.join(BASE_DIR, "testapp", "commands")).replace('\\', '/')
 ]
-
-##### CELERY SETTINGS
-
-# CELERY_BROKER_URL = "redis://localhost:6379"
-# BROKER_URL = 'redis://127.0.0.1:6379'
-# CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
-# CELERY_ACCEPT_CONTENT = ["pickle", "json"]
-# CELERY_TASK_SERIALIZER = "pickle"
-# CELERY_RESULT_SERIALIZER = "pickle"
-
-
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_BROKER_URL = BROKER_URL = 'amqp://guest:guest@localhost'
-
-if CELERY_BROKER_URL.startswith('sqs://'):
-    CELERY_BROKER_TRANSPORT_OPTIONS = {
-        'visibility_timeout': 43200,
-        'polling_interval': 1.0,
-        'queue_name_prefix': 'testapp_'
-    }
-
-CELERY_TASK_ACKS_LATE = True
-CELERY_WORKER_PREFETCH_MULTIPLIER = 1
+DJANGO_COMMANDER_USE_S3 = False
