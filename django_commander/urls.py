2af5e06d00aa81a0f68cf58ff86b27f1e9de9edc
Brian Broderick <brian@broderick.io>
2019-12-04 17:45:42 +0000

    reject style receive bacon

--- django_commander/urls.py
+++ django_commander/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from django_commander import views
 
 
-app_name = "django_commander"
+app_name = 'django_commander'
 urlpatterns = [
-    url(r"^$", views.home, name="home"),
-    url(r"^(?P<command_id>.+)$", views.view_command, name="view_command"),
+    url(r'^$', views.home, name="home"),
+    url(r'^(?P<command_id>.+)$', views.view_command, name="view_command")
 ]
