9a4e52b0282dea5ad7295e4560893660c44cc37f
pvankessel <pvankessel@pewresearch.org>
2021-08-23 12:49:32 -0400

    simplifying caching with a single DJANGO_COMMANDER_CACHE_PATH setting; the code was already using the S3 path for both local and S3 caching based on the DJANGO_COMMANDER_USE_S3 setting so it makes sense to just have a single path combined with the DJANGO_COMMANDER_USE_S3 toggle

--- django_commander/apps.py
+++ django_commander/apps.py
@@ -17,20 +17,16 @@ class DjangoCommanderConfig(AppConfig):
             ("AWS_ACCESS_KEY_ID", None),
             ("AWS_SECRET_ACCESS_KEY", None),
             ("S3_BUCKET", None),
-            ("LOCAL_CACHE_ROOT", "cache"),
-            ("S3_CACHE_ROOT", "cache"),
+            ("DJANGO_COMMANDER_CACHE_PATH", "cache"),
             ("DJANGO_COMMANDER_USE_S3", False),
         ]:
             if not hasattr(settings, setting):
                 setattr(settings, setting, default)
 
-        LOCAL_CACHE_PATH = os.path.join(settings.LOCAL_CACHE_ROOT, "django_queries")
-        setattr(settings, "LOCAL_CACHE_PATH", LOCAL_CACHE_PATH)
-        if settings.DJANGO_COMMANDER_USE_S3:
-            S3_CACHE_PATH = os.path.join(settings.S3_CACHE_ROOT, "django_queries")
-        else:
-            S3_CACHE_PATH = settings.LOCAL_CACHE_PATH
-        setattr(settings, "S3_CACHE_PATH", S3_CACHE_PATH)
+        DJANGO_COMMANDER_CACHE_PATH = os.path.join(
+            settings.DJANGO_COMMANDER_CACHE_PATH, "django_commander"
+        )
+        setattr(settings, "DJANGO_COMMANDER_CACHE_PATH", DJANGO_COMMANDER_CACHE_PATH)
 
         templates = settings.TEMPLATES
         new_templates = []
