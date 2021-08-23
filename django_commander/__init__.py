bb98103545ba4ad9afd5d4d3112f1c04aca9c659
pvankessel <pvankessel@pewresearch.org>
2021-08-23 12:49:54 -0400

    removing VERSION file like the other repos

--- django_commander/__init__.py
+++ django_commander/__init__.py
@@ -1,7 +1,4 @@
 from builtins import str
 import os
 
-with open(os.path.join(os.path.dirname(__file__), "VERSION"), "rb") as version_file:
-    __version__ = str(version_file.read()).strip()
-
 default_app_config = "django_commander.apps.DjangoCommanderConfig"
