9dfe5dcf146b3ce2cd4a23bfebb5c84ab029ee39
Brian Broderick <brian@broderick.io>
2022-01-26 13:06:18 +0000

    Bump version: 0.1.5.dev1 → 0.1.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("requirements.txt") as reqs:
 
 setup(
     name="django_commander",
-    version="0.1.5.dev1",
+    version="0.1.5",
     description="Easily create, organize, and log management commands and subcommands",
     long_description=README,  # 'http://labs.pewresearch.tech/docs/libs/django_commander',
     url="https://github.com/pewresearch/django_commander",
